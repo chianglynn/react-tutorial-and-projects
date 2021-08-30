f9001042c1d0f13fe3e1e193cd1ef024f7c547e5
Chiang Lynn <lynnchiangtw@gmail.com>
2021-08-30 20:49:02 +0800

    Upload starter files

---
+++ 13-stripe-submenus/src/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import logo from './images/logo.svg';
+
+const Navbar = () => {
+  return <h2>navbar component</h2>
+};
+
+export default Navbar;
