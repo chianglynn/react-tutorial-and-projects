6b637376797c03bff95a5af4e825283a5f8a5aca
Chiang Lynn <lynnchiangtw@gmail.com>
2021-08-31 23:33:20 +0800

    Finish cart project

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,28 +1,52 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react';
+import React, { useContext, useReducer, useEffect } from 'react';
 import cartItems from './data';
 import reducer from './reducer';
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-useReducer-cart-project';
+
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchdata = async () => {
+    dispatch({ type: 'LOADING' });
+    const response = await fetch(url);
+    const data = await response.json();
+    dispatch({ type: 'DISPLAY_ITEMS', payload: data });
+
+  };
+  const clearCart = () => dispatch({ type: 'CLEAR_CART' });
+  const remove = (id) => dispatch({ type: 'REMOVE', payload: id });
+  // const increase = (id) => dispatch({ type: 'INCREASE', payload: id });
+  // const decrease = (id) => dispatch({ type: 'DECREASE', payload: id });
+  const toggleAmount = (id, type) => { dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } }) };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  useEffect(() => dispatch({ type: 'GET_TOTALS' }), [state.cart]);
 
   return (
-    <AppContext.Provider
-      value={{
-        cart,
-      }}
-    >
+    <AppContext.Provider value={{
+      ...state,
+      clearCart,
+      remove,
+      // increase, decrease,
+      toggleAmount
+    }}>
       {children}
     </AppContext.Provider>
   );
 }
 // make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-};
+export const useGlobalContext = () => useContext(AppContext);
 
 export { AppContext, AppProvider };
