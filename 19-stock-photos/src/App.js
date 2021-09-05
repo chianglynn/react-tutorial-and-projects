621d57e1c685ce37b80b4b9b8991dd5bdf55a6b9
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-05 23:23:36 +0800

    Finish stock photos project

--- 19-stock-photos/src/App.js
+++ 19-stock-photos/src/App.js
@@ -1,13 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
 
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(true);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+  };
+
+  const fetchImages = useCallback(async () => {
+    setLoading(true);
+
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    let url;
+
+    if (query) url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    else url = `${mainUrl}${clientID}${urlPage}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos(oldPhotos => {
+        if (query && page === 1) return data.results;
+        else if (query && page !== 1) return [...oldPhotos, ...data.results];
+        else return [...oldPhotos, ...data];
+      });
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [page, fetchImages]);
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if ((!loading && window.innerHeight + window.scrollY) >= document.body.scrollHeight - 2) setPage(oldPage => oldPage + 1);
+    });
+    return () => window.removeEventListener('scroll', event);
+  }, [loading]);
+
+  return (
+    <main>
+      <section className="search" onSubmit={handleSubmit}>
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button type="submit" className="submit-btn" ><FaSearch /></button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo, index) => <Photo key={index} {...photo} />)}
+        </div>
+      </section>
+      {loading && <h2 className="loading">loading...</h2>}
+    </main>
+  );
 }
 
 export default App;
