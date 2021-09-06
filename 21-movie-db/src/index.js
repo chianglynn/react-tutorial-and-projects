bddefd66d4bed95b12924a10ad1279493603319c
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-06 13:39:26 +0800

    Upload starter files

---
+++ 21-movie-db/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import { AppProvider } from './context';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
