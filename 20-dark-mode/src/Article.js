f09694b4783968dcf41dd8facae4ff3b22566264
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-06 13:35:07 +0800

    Finish dark mode project

--- 20-dark-mode/src/Article.js
+++ 20-dark-mode/src/Article.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ title, date, length, snippet }) => {
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format('MMMM Do YYYY')}</span>
+        <span>{length} mins read</span>
+        <span>{snippet}</span>
+      </div>
+    </article>
+  );
 };
 
 export default Article;
