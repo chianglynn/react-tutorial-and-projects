fbf90d061da0dfbd272e430fe5aacf2d7e54512a
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-11 14:03:11 +0800

    Set up info page

--- 24-github-search-users/src/components/Info.js
+++ 24-github-search-users/src/components/Info.js
@@ -5,7 +5,36 @@ import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { public_repos, public_gists, followers, followering } = githubUser;
+  const items = [
+    { id: 1, icon: <GoRepo className="icon" />, label: 'repos', value: public_repos, color: 'pink' },
+    { id: 2, icon: <FiUsers className="icon" />, label: 'followers', value: followers, color: 'green' },
+    { id: 3, icon: <FiUserPlus className="icon" />, label: 'followering', value: followering, color: 'purple' },
+    { id: 4, icon: <GoGist className="icon" />, label: 'gists', value: public_gists, color: 'yellow' },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {items.map(item => {
+          return <Item key={item.id} {...item} />;
+        })}
+      </Wrapper>
+    </section>
+  );
+};
+
+const Item = ({ icon, label, value, color }) => {
+  return (
+    <article className="item">
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value ? value : 0}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  );
 };
 
 const Wrapper = styled.section`
